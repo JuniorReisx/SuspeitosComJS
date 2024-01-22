19c1c075a2762bc03e5187301ec0d54066b4452a
Junior Reis <gilmaikjunior@gmail.com>
2024-01-22 17:13:05 -0300

    SuspeitosJS

---
+++ script.js
@@ -0,0 +1,22 @@
+let suspeitol = "Não sou eu!!"
+let suspeito2 = "Eu tentei de ajudar desde o inicio!!"
+let suspeito3 = "Já pensou que o monstro pode ser controlado"
+
+if(suspeito1.length > suspeito2.length && suspeito1.length > suspeito3.length ){
+    
+    console.log("É o suspeito 1 com " + suspeito1.length)
+    console.log('Com a frase "' + suspeitol + '"')}
+
+
+else if(suspeito2.length > suspeito1.length && suspeito2.length> suspeito3.length){
+      console.log("É o suspeito 2 com " + suspeito2.length)
+      console.log('Com a frase "' + suspeito2 + '"')
+
+} 
+
+else{
+
+      console.log("É o suspeito 3 com "+suspeito3.length)
+      console.log('Com a frase ' + suspeito3 + '"')
+
+}
